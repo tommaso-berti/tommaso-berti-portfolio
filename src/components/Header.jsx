1be92f015ed0961a994c67968ade7e01bc5cce4e
Tommaso Berti <tommaso.coding@gmail.com>
2025-11-14 08:30:41 +0100

    Add language toggle to header and refactor layout with Stack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-import Box from '@mui/material/Box';
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 import BreadCrumbs from './BreadCrumbs.jsx';
 import DarkModeToggle from "./DarkModeToggle.jsx";
+import TranslateIcon from '@mui/icons-material/Translate';
+import IconButton from '@mui/material/IconButton'
 
 export default function Header() {
     return (
@@ -16,7 +19,12 @@ export default function Header() {
             }}
         >
             <BreadCrumbs />
-            <DarkModeToggle />
+            <Stack direction="row">
+                <IconButton>
+                    <TranslateIcon />
+                </IconButton>
+                <DarkModeToggle />
+            </Stack>
         </Box>
     )
 }
